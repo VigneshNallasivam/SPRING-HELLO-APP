44f65386db84ae35a738600560b0b8b1f2aa91c3
VigneshNallasivam <vickyshanestark@gmail.com>
2023-02-09 12:41:05 +0530

    Added : Creation Spring-boot Hello World App

---
+++ src/main/java/com/sample/hello/HelloSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.sample.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sample/hello/HelloSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
